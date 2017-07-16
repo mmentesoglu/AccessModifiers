dc3be0ab1312901ef88fe27318840dd08af605a0
mustafa.mentesoglu <mustafa.mentesoglu@SMART0617.cardtek.local>
2017-07-16 12:36:12 +0300

    Add StoryBook.java Add House.java

---
+++ src/building/House.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package building;
+
+import library.Book;
+
+/**
+ *
+ * @author mustafa.mentesoglu
+ */
+public class House {
+
+    public House() {
+        Book book = new Book();
+        
+        //book.author = "ABC"; //Can'not access
+        //book.modifyTemplate(); //Can'not access
+        
+    }
+    
+}
