d6506d5daec62d60ba5f054af6da28dbfae6603b
mustafa.mentesoglu <mustafa.mentesoglu@SMART0617.cardtek.local>
2017-07-16 12:22:08 +0300

    Add Librarian.Java

---
+++ src/library/Librarian.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+/**
+ *
+ * @author mustafa.mentesoglu
+ */
+public class Librarian {
+
+    public Librarian() {
+        
+    }
+    
+}
